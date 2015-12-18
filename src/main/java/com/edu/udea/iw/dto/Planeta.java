c923727fc53e17c4640b3b498004bf5bed11dcea
System User <thedemonsspeed@gmail.com>
2015-12-18 15:59:08 -0500

    añadida estructura de paquetes de java

---
+++ src/main/java/com/edu/udea/iw/dto/Planeta.java
@@ -0,0 +1,14 @@
+package com.edu.udea.iw.dto;
+
+import java.io.Serializable;
+
+public class Planeta implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -178767740576164371L;
+
+	
+	
+}
