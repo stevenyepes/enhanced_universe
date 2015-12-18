a1fd4a9524d23ed81f27c0d55751a017cb24604b
System User <cara994923@gmail.com>
2015-12-18 17:15:43 -0500

    Agrego comentarios al DTO Cometa

--- src/main/java/com/edu/udea/iw/dto/Cometa.java
+++ src/main/java/com/edu/udea/iw/dto/Cometa.java
@@ -2,19 +2,44 @@ package com.edu.udea.iw.dto;
 
 import java.io.Serializable;
 
+/**
+ * Clase con el patron DTO para transportar los datos de los cometas
+ * @author enhanced_universe
+ *
+ */
 public class Cometa implements Serializable {
 
 	/**
-	 * 
+	 * Serial recomendado por java para una clase que implementa la Interface Serializable 
 	 */
 	private static final long serialVersionUID = 5818229292591093223L;
-
+	
+	/**
+	 * Id del cometa
+	 */
 	private Integer id;
+	/**
+	 * Nombre completo del cometa 
+	 */
 	private String nombre;
+	/**
+	 * Diametro del cometa en kilometros 
+	 */
 	private Double diametro;
+	/**
+	 * Tiempo que tarda la orbita del cometa 
+	 */
 	private Double periodoorbital;
+	/**
+	 * Ultimo año en el cual estubo cerca el cometa
+	 */
 	private Integer ultimoperihelio;
+	/**
+	 * Galaxia en la cual se mueve el cometa
+	 */
 	private Galaxia galaxia;
+	
+	
 	public Integer getId() {
 		return id;
 	}
@@ -50,7 +75,6 @@ public class Cometa implements Serializable {
 	}
 	public void setGalaxia(Galaxia galaxia) {
 		this.galaxia = galaxia;
-	}
-	
+	}	
 	
 }
