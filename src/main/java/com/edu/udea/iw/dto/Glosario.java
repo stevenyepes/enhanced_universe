739385dbd441db337fef66da62ea37e2cd68b67d
System User <cara994923@gmail.com>
2015-12-18 18:33:05 -0500

    Comentarios a DAO

--- src/main/java/com/edu/udea/iw/dto/Glosario.java
+++ src/main/java/com/edu/udea/iw/dto/Glosario.java
@@ -2,16 +2,31 @@ package com.edu.udea.iw.dto;
 
 import java.io.Serializable;
 
+/**
+ * Clase con el patron DTO para transportar los datos del glosario
+ * @author enhanced_universe
+ *
+ */
 public class Glosario implements Serializable{
 
 	/**
-	 * 
+	 * Serial recomendado por java para una clase que implementa la Interface Serializable 
 	 */
 	private static final long serialVersionUID = -8623640273954487550L;
 	
+	/**
+	 * Id del glosario 
+	 */
 	private Integer id;
+	/**
+	 * Termino a definir en el glosario 
+	 */
 	private String termino;
+	/**
+	 * Definicion del termino en el glosario 
+	 */
 	private String definicion;
+	
 	
 	public Integer getId() {
 		return id;
