2c96b1ae2c6c8191240e91a8d6f053d1f6395f0f
System User <cara994923@gmail.com>
2015-12-18 19:48:29 -0500

    Comentarios a DAO

--- src/main/java/com/edu/udea/iw/dao/EstrellaDAO.java
+++ src/main/java/com/edu/udea/iw/dao/EstrellaDAO.java
@@ -5,12 +5,54 @@ import java.util.List;
 import com.edu.udea.iw.dto.Estrella;
 import com.edu.udea.iw.exception.MyException;
 
+/**
+ * Interface que define los metodos que va a proveer el estrella
+ * @author enhanced_universe
+ *
+ */
 public interface EstrellaDAO {
 	
+	/**
+	 * Entrega la lista de estrellas en la base de datos
+	 * @return lista estrellas
+	 * @throws MyException Ocurre un error con la conexión.
+	 */
 	public List<Estrella> consultar() throws MyException;
+	
+	/**
+	 * Consulta los datos de una estrella dado su nombre.
+	 * @param nombre nombre de la estrella a consultar
+	 * @return una instacia de los datos de la estrella consultada, si no
+	 * existe una estrella con el nombre dado retorna un null.
+	 * @throws MyException Ocurre un error con la base de datos.
+	 */
 	public Estrella consultaUnica(String nombre) throws MyException;
+	
+	/**
+	 * Guarda los datos de estrella dada.
+	 * @param estrella estrella a guardar
+	 * @return true si la estrella se guardo con exito, false
+	 * si la galaxia no guardo correctamente 
+	 * @throws MyException Ocurre un error con la base de datos.
+	 */
 	public Boolean guardar(Estrella estrella) throws MyException;
+	
+	/**
+	 * Actualiza los datos de estrella dada.
+	 * @param estrella estrella a actualizar
+	 * @return true si la estrella se actualiza con exito, false
+	 * si la estrella no actualiza correctamente 
+	 * @throws MyException Ocurre un error con la base de datos.
+	 */
 	public Boolean actualizar(Estrella estrella) throws MyException;
+	
+	/**
+	 * Elimina los datos de estrella dada.
+	 * @param estrella estrella a eliminar
+	 * @return true si la estrella se elimino con exito, false
+	 * si la estrella no elimino correctamente 
+	 * @throws MyException Ocurre un error con la base de datos.
+	 */
 	public Boolean eliminar(Estrella estrella) throws MyException;
 
 }
--- src/main/java/com/edu/udea/iw/dao/TipoEspectralDAO.java
+++ src/main/java/com/edu/udea/iw/dao/TipoEspectralDAO.java
@@ -5,9 +5,35 @@ import java.util.List;
 import com.edu.udea.iw.dto.TipoEspectral;
 import com.edu.udea.iw.exception.MyException;
 
+/**
+ * Interface que define los metodos que va a proveer el dao tipo espectral
+ * @author enhanced_universe
+ *
+ */
 public interface TipoEspectralDAO {
-
+	
+	/**
+	 * Entrega la lista de tipo espectral en la base de datos
+	 * @return lista tiposespectrales
+	 * @throws MyException Ocurre un error con la conexión.
+	 */
 	public List<TipoEspectral> consultar() throws MyException;
+	
+	/**
+	 * Guarda los datos de tipo espectral dado.
+	 * @param tipoEspectral tipoEspectral a guardar
+	 * @return true si el tipoEspectral se guardo con exito, false
+	 * si el tipoEspectral no guardo correctamente 
+	 * @throws MyException Ocurre un error con la base de datos.
+	 */
 	public Boolean guardar(TipoEspectral tipoEspectral) throws MyException;
+	
+	/**
+	 * Elimina los datos del tipo espectral dado.
+	 * @param tipoEspectral tipo espectral a eliminar
+	 * @return true si el tipoEspectral se elimino con exito, false
+	 * si el tipoEspectral no elimino correctamente 
+	 * @throws MyException Ocurre un error con la base de datos.
+	 */
 	public Boolean eliminar(TipoEspectral tipoEspectral) throws MyException;
 }
